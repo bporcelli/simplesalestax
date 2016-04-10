c4166531b6bc711e7186b1ef0c0ab6095bfa24c7
Brett Porcelli <brettporcelli@gmail.com>
2016-04-10 07:40:04 -0400

    Revert "Changed plugin name from WooTax to Simple Sales Tax. Removed all occurrences of "WooTax" in messages displayed to users. Eliminated unused methods in admin JS. Use redirect to route users to TaxCloud reports page."
    This reverts commit 806b87f1675c8812d5c33a2919a72a6277ecf2d7.

--- templates/admin/rate-table-row.php
+++
@@ -1,7 +0,0 @@
-<?php global $rate, $data; ?>
-
-<tr class="flat_rate">
-	<th class="check-column"><input type="checkbox" data-val="<?php echo $rate; ?>" name="select"></th>
-	<td><?php echo $data['name']; ?></td>
-	<td><?php echo $data['count']; ?></td>
-</tr>
