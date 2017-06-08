aef4204a84c117942bc6994a462243fb61f239ea
Brett Porcelli <brettporcelli@gmail.com>
2017-06-08 15:24:51 -0400

    Resolves #62

--- includes/admin/views/html-notice-update.php
+++ includes/admin/views/html-notice-update.php
@@ -9,4 +9,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 <p><strong><?php _e( 'Simple Sales Tax data update.', 'simplesalestax' ); ?></strong> <?php _e( 'We need to update your database to the latest version.', 'simplesalestax' ); ?></p>
-<p class="submit"><a href="<?php echo esc_url( add_query_arg( 'do_sst_update', 'true' ) ); ?>" class="wc-update-now button-primary"><?php _e( 'Run the updater', 'simplesalestax' ); ?></a></p>
+<p class="submit"><a href="<?php echo esc_url( admin_url( '?do_sst_update=true' ) ); ?>" class="wc-update-now button-primary"><?php _e( 'Run the updater', 'simplesalestax' ); ?></a></p>
