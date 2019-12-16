6dc1afdd99eb282131f7fa68a773f426d4e57f14
Brett Porcelli <brettporcelli@gmail.com>
2019-12-16 13:19:23 -0500

    chore: update plugin headers for WP.org
    Update author and copyright information and add license disclaimer.

--- simplesalestax.php
+++ simplesalestax.php
@@ -2,21 +2,36 @@
 
 /**
  * Plugin Name:          Simple Sales Tax
- * Plugin URI:           https://simplesalestax.com
  * Description:          Harness the power of TaxCloud to accurately calculate sales tax for your WooCommerce store.
- * Author:               Simple Sales Tax
- * Author URI:           https://simplesalestax.com
+ * Author:               TaxCloud
+ * Author URI:           https://taxcloud.com
+ * GitHub Plugin URI:    https://github.com/bporcelli/simplesalestax
  * Version:              6.0.10
+ * Text Domain:          simplesalestax
+ * Domain Path:          /languages/
+ *
  * Requires at least:    4.5.0
  * Tested up to:         5.3.0
  * WC requires at least: 3.0.0
  * WC tested up to:      3.8.0
- * Text Domain:          simplesalestax
- * Domain Path:          /languages
- * License:              GPL-3.0+
- * License URI:          https://www.gnu.org/licenses/gpl-3.0.txt
  *
- * @copyright FedTax (email: support@simplesalestax.com)
+ * @category             Plugin
+ * @copyright            Copyright © 2019 The Federal Tax Authority, LLC
+ * @author               Brett Porcelli
+ * @license              GPL2
+ *
+ * Simple Sales Tax is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 2 of the License, or any later
+ * version.
+ *
+ * Simple Sales Tax is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Simple Sales Tax. If not, see http://www.gnu.org/licenses/gpl-2.0.txt.
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
