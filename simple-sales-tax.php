11823ceb24c6a8f3d760599526bf2c06c7fae17d
Brett Porcelli <brettporcelli@gmail.com>
2020-03-15 11:52:35 -0400

    chore: bump version to 6.0.12 and update tested WP and WC versions

--- simple-sales-tax.php
+++ simple-sales-tax.php
@@ -6,14 +6,14 @@
  * Author:               TaxCloud
  * Author URI:           https://taxcloud.com
  * GitHub Plugin URI:    https://github.com/bporcelli/simplesalestax
- * Version:              6.0.11
+ * Version:              6.0.12
  * Text Domain:          simple-sales-tax
  * Domain Path:          /languages/
  *
  * Requires at least:    4.5.0
- * Tested up to:         5.3.0
+ * Tested up to:         5.4.0
  * WC requires at least: 3.0.0
- * WC tested up to:      3.9.0
+ * WC tested up to:      4.0.0
  *
  * @category             Plugin
  * @copyright            Copyright © 2020 The Federal Tax Authority, LLC
