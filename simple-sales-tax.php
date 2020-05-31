03b37d4f8e35d4a21b261e424115cc0f3d3e95c6
Brett Porcelli <brettporcelli@gmail.com>
2020-05-31 12:14:09 -0400

    chore: declare support for WC 4.2

--- simple-sales-tax.php
+++ simple-sales-tax.php
@@ -13,7 +13,7 @@
  * Requires at least:    4.5.0
  * Tested up to:         5.4.0
  * WC requires at least: 3.0.0
- * WC tested up to:      4.0.0
+ * WC tested up to:      4.2.0
  *
  * @category             Plugin
  * @copyright            Copyright © 2020 The Federal Tax Authority, LLC
