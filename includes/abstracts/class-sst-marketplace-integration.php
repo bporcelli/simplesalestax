d1722fa8fb0089e44d3ebef587c2b121bb74c7b2
Brett Porcelli <brettporcelli@gmail.com>
2020-11-13 15:50:05 -0500

    refactor: move package filters to marketplace integration base class
    wip #58

--- includes/abstracts/class-sst-marketplace-integration.php
+++ includes/abstracts/class-sst-marketplace-integration.php
@@ -17,9 +17,36 @@ if ( ! defined( 'ABSPATH' ) ) {
 abstract class SST_Marketplace_Integration {
 
     /**
+     * Key used to save seller ID in cart packages.
+     *
+     * @var string
+     */
+    protected $cart_seller_id_key = 'vendor_id';
+
+    /**
+     * Key used to save seller ID in order shipping items.
+     *
+     * @var string
+     */
+    protected $order_seller_id_key = 'vendor_id';
+
+    /**
+     * Flag to control whether package filters should be added.
+     *
+     * @var bool
+     */
+    protected $should_add_package_filters = true;
+
+    /**
      * Constructor.
      */
     public function __construct() {
+        if ( ! $this->should_add_package_filters ) {
+            return;
+        }
+
+        add_filter( 'wootax_cart_packages_before_split', array( $this, 'filter_wootax_cart_packages' ), 10, 2 );
+        add_filter( 'wootax_order_packages_before_split', array( $this, 'filter_wootax_order_packages' ), 10, 2 );
     }
 
     /**
@@ -42,5 +69,132 @@ abstract class SST_Marketplace_Integration {
             update_post_meta( $product_id, 'wootax_tic', sanitize_text_field( $tic ) );
         }
     }
+
+    /**
+     * Filters the cart packages so that we execute one tax lookup per seller.
+     *
+     * @param array   $packages SST cart packages.
+     * @param WC_Cart $cart     Cart instance.
+     *
+     * @return array
+     */
+    public function filter_wootax_cart_packages( $packages, $cart ) {
+        $packages = array();
+
+        // There should be one package per seller. We'll loop over each package
+        // and add in any of the seller's virtual/downloadable products since
+        // these aren't included in shipping packages by default.
+        $shipping_packages = WC()->shipping->get_packages();
+        $seller_id_key     = $this->cart_seller_id_key;
+
+        foreach ( $shipping_packages as $key => $package ) {
+            $seller_id = isset( $package[ $seller_id_key ] ) ? $package[ $seller_id_key ] : 0;
+
+            // Add in virtual products not needing shipping.
+            $virtual_products = array();
+
+            foreach ( $cart->get_cart() as $cart_item_key => $cart_item ) {
+                if ( isset( $cart_item['data'] ) && ! $cart_item['data']->needs_shipping() ) {
+                    $virtual_products[ $cart_item_key ] = $cart_item;
+                }
+            }
+
+            if ( $virtual_products ) {
+                $package['contents']      += $virtual_products;
+                $package['contents_cost'] += array_sum( wp_list_pluck( $virtual_products, 'line_total' ) );
+            }
+
+            // Set origin address based on seller's store address if package is
+            // for seller.
+            if ( $this->is_user_seller( $seller_id ) ) {
+                $package['origin'] = $this->get_seller_address( $seller_id );
+            }
+
+            $packages[ $key ] = $package;
+        }
+
+        return $packages;
+    }
+
+    /**
+     * Filters the order packages so that we can execute one tax lookup per seller.
+     *
+     * @param array    $packages SST order packages.
+     * @param WC_Order $order    Order instance.
+     */
+    public function filter_wootax_order_packages( $packages, $order ) {
+        $packages           = array();
+        $items_by_seller    = array();
+        $shipping_by_seller = array();
+        $order_items        = sst_format_order_items( $order->get_items() );
+
+        foreach ( $order_items as $item_id => $item ) {
+            $seller_id = get_post_field( 'post_author', $item['product_id'] );
+            if ( ! isset( $items_by_seller[ $seller_id ] ) ) {
+                $items_by_seller[ $seller_id ] = array();
+            }
+            $items_by_seller[ $seller_id ][ $item_id ] = $item;
+        }
+
+        // Assumes one shipping method per seller.
+        $seller_id_key = $this->order_seller_id_key;
+
+        foreach ( $order->get_shipping_methods() as $item_id => $shipping_method ) {
+            $seller_id                        = $shipping_method->get_meta( $seller_id_key );
+            $shipping_by_seller[ $seller_id ] = $shipping_method;
+        }
+
+        $seller_ids = array_keys( $shipping_by_seller + $items_by_seller );
+
+        // Create one package per seller.
+        foreach ( $seller_ids as $seller_id ) {
+            $seller_package = array(
+                'user' => array(
+                    'ID' => $order->get_user_id(),
+                ),
+            );
+
+            if ( isset( $items_by_seller[ $seller_id ] ) ) {
+                $seller_package['contents'] = $items_by_seller[ $seller_id ];
+            }
+
+            if ( isset( $shipping_by_seller[ $seller_id ] ) ) {
+                $shipping_method            = $shipping_by_seller[ $seller_id ];
+                $seller_package['shipping'] = new WC_Shipping_Rate(
+                    $shipping_method->get_id(),
+                    '',
+                    $shipping_method->get_total(),
+                    array(),
+                    $shipping_method->get_method_id()
+                );
+            }
+
+            if ( $this->is_user_seller( $seller_id ) ) {
+                $seller_package['origin'] = $this->get_seller_address( $seller_id );
+            }
+
+            $packages[] = sst_create_package( $seller_package );
+        }
+
+        return $packages;
+    }
+
+    /**
+     * Checks whether a user is a seller.
+     *
+     * @param int $user_id User ID.
+     *
+     * @return bool
+     */
+    abstract public function is_user_seller( $user_id );
+
+    /**
+     * Get the origin address for a seller.
+     *
+     * @param int $seller_id Seller user ID.
+     *
+     * @return SST_Origin_Address
+     */
+    abstract public function get_seller_address( $seller_id );
 
 }
--- includes/integrations/class-sst-dokan.php
+++ includes/integrations/class-sst-dokan.php
@@ -42,6 +42,20 @@ class SST_Dokan extends SST_Marketplace_Integration {
 	protected $printed_tic_select = false;
 
 	/**
+     * Key used to save seller ID in cart packages.
+     *
+     * @var string
+     */
+    protected $cart_seller_id_key = 'seller_id';
+
+    /**
+     * Key used to save seller ID in order shipping items.
+     *
+     * @var string
+     */
+    protected $order_seller_id_key = 'seller_id';
+
+	/**
 	 * Returns the singleton instance of this class.
 	 *
 	 * @return SST_Dokan
@@ -89,8 +103,6 @@ class SST_Dokan extends SST_Marketplace_Integration {
 		add_action( 'wp_footer', array( $this, 'print_styles_to_hide_tax_class_field' ) );
 		add_filter( 'wootax_tic_select_init_events', array( $this, 'filter_tic_select_init_events' ) );
 		add_filter( 'wootax_marketplace_is_user_seller', array( $this, 'is_user_seller' ) );
-		add_filter( 'wootax_cart_packages_before_split', array( $this, 'filter_wootax_cart_packages' ), 10, 2 );
-		add_filter( 'wootax_order_packages_before_split', array( $this, 'filter_wootax_order_packages' ), 10, 2 );
 		add_action( 'sst_output_tax_meta_box', array( $this, 'output_tax_meta_box' ) );
 		add_action( 'dokan_checkout_update_order_meta', array( $this, 'recalculate_sub_order_taxes' ) );
 		add_filter( 'sst_should_capture_order', array( $this, 'prevent_parent_order_processing' ), 10, 2 );
@@ -225,112 +237,6 @@ class SST_Dokan extends SST_Marketplace_Integration {
 	 */
 	public function is_user_seller( $user_id ) {
 		return dokan_is_user_seller( $user_id );
-	}
-
-	/**
-	 * Filters the cart packages so that we execute one tax lookup per seller.
-	 *
-	 * @param array   $packages SST cart packages.
-	 * @param WC_Cart $cart     Cart instance.
-	 *
-	 * @return array
-	 */
-	public function filter_wootax_cart_packages( $packages, $cart ) {
-		$packages = array();
-
-		// There should be one package per seller. We'll loop over each package
-		// and add in any of the seller's virtual/downloadable products since
-		// these aren't included in shipping packages by default.
-		$shipping_packages = WC()->shipping->get_packages();
-
-		foreach ( $shipping_packages as $key => $package ) {
-			$seller_id = isset( $package['seller_id'] ) ? $package['seller_id'] : 0;
-
-			// Add in virtual products not needing shipping.
-			$virtual_products = array();
-
-			foreach ( $cart->get_cart() as $cart_item_key => $cart_item ) {
-				if ( isset( $cart_item['data'] ) && ! $cart_item['data']->needs_shipping() ) {
-					$virtual_products[ $cart_item_key ] = $cart_item;
-				}
-			}
-
-			if ( $virtual_products ) {
-				$package['contents']      += $virtual_products;
-				$package['contents_cost'] += array_sum( wp_list_pluck( $virtual_products, 'line_total' ) );
-			}
-
-			// Set origin address based on seller's store address if package is
-			// for seller.
-			if ( $this->is_user_seller( $seller_id ) ) {
-				$package['origin'] = $this->get_seller_address( $seller_id );
-			}
-
-			$packages[ $key ] = $package;
-		}
-
-		return $packages;
-	}
-
-	/**
-	 * Filters the order packages so that we can execute one tax lookup per seller.
-	 *
-	 * @param array    $packages SST order packages.
-	 * @param WC_Order $order    Order instance.
-	 */
-	public function filter_wootax_order_packages( $packages, $order ) {
-		$packages           = array();
-		$items_by_seller    = array();
-		$shipping_by_seller = array();
-		$order_items        = sst_format_order_items( $order->get_items() );
-
-		foreach ( $order_items as $item_id => $item ) {
-			$seller_id = get_post_field( 'post_author', $item['product_id'] );
-			if ( ! isset( $items_by_seller[ $seller_id ] ) ) {
-				$items_by_seller[ $seller_id ] = array();
-			}
-			$items_by_seller[ $seller_id ][ $item_id ] = $item;
-		}
-
-		// Assumes one shipping method per seller.
-		foreach ( $order->get_shipping_methods() as $item_id => $shipping_method ) {
-			$seller_id                        = $shipping_method->get_meta( 'seller_id' );
-			$shipping_by_seller[ $seller_id ] = $shipping_method;
-		}
-
-		$seller_ids = array_keys( $shipping_by_seller + $items_by_seller );
-
-		// Create one package per seller.
-		foreach ( $seller_ids as $seller_id ) {
-			$seller_package = array(
-				'user' => array(
-					'ID' => $order->get_user_id(),
-				),
-			);
-
-			if ( isset( $items_by_seller[ $seller_id ] ) ) {
-				$seller_package['contents'] = $items_by_seller[ $seller_id ];
-			}
-
-			if ( isset( $shipping_by_seller[ $seller_id ] ) ) {
-				$shipping_method            = $shipping_by_seller[ $seller_id ];
-				$seller_package['shipping'] = new WC_Shipping_Rate(
-					$shipping_method->get_id(),
-					'',
-					$shipping_method->get_total(),
-					array(),
-					$shipping_method->get_method_id()
-				);
-			}
-
-			if ( $this->is_user_seller( $seller_id ) ) {
-				$seller_package['origin'] = $this->get_seller_address( $seller_id );
-			}
-
-			$packages[] = sst_create_package( $seller_package );
-		}
-
-		return $packages;
 	}
 
 	/**
