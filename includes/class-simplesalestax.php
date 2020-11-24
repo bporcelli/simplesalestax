6fd81e00cf86a640205db7e7fb927563afdbe455
Brett Porcelli <brettporcelli@gmail.com>
2020-11-23 19:37:58 -0500

    chore: prep for 6.2.3 release

--- includes/class-simplesalestax.php
+++ includes/class-simplesalestax.php
@@ -20,7 +20,7 @@ final class SimpleSalesTax {
 	 *
 	 * @var string
 	 */
-	public $version = '6.2.2';
+	public $version = '6.2.3';
 
 	/**
 	 * The singleton plugin instance.
--- simple-sales-tax.php
+++ simple-sales-tax.php
@@ -6,7 +6,7 @@
  * Author:               TaxCloud
  * Author URI:           https://taxcloud.com
  * GitHub Plugin URI:    https://github.com/bporcelli/simplesalestax
- * Version:              6.2.2
+ * Version:              6.2.3
  * Text Domain:          simple-sales-tax
  * Domain Path:          /languages/
  *
