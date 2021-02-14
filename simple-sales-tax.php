d786e468722daee3dd3ff750ccaf14c8a37c9955
Brett Porcelli <brettporcelli@gmail.com>
2021-02-14 10:56:22 -0500

    chore: update tested WC version to 5.0

--- simple-sales-tax.php
+++ simple-sales-tax.php
@@ -13,7 +13,7 @@
  * Requires at least:    4.5.0
  * Tested up to:         5.6.0
  * WC requires at least: 3.0.0
- * WC tested up to:      4.9.0
+ * WC tested up to:      5.0.0
  *
  * @category             Plugin
  * @copyright            Copyright © 2020 The Federal Tax Authority, LLC
