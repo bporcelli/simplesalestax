2deb24f53b78f08fa39bd7dd9f76d8b9b189fdeb
Brett Porcelli <brettporcelli@gmail.com>
2022-05-07 11:48:50 -0400

    chore: updated tested WP version to 6.0

--- simple-sales-tax.php
+++ simple-sales-tax.php
@@ -11,7 +11,7 @@
  * Domain Path:          /languages/
  *
  * Requires at least:    4.5.0
- * Tested up to:         5.9.0
+ * Tested up to:         6.0.0
  * WC requires at least: 3.0.0
  * WC tested up to:      6.4.0
  *
