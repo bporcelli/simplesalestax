0e6ce7e3e2089cad4be7b507b4067a7507746d19
Brett Porcelli <brettporcelli@gmail.com>
2022-10-14 11:46:01 -0400

    chore: prep for 7.0.2 release

--- simple-sales-tax.php
+++ simple-sales-tax.php
@@ -6,7 +6,7 @@
  * Author:               TaxCloud
  * Author URI:           https://taxcloud.com
  * GitHub Plugin URI:    https://github.com/bporcelli/simplesalestax
- * Version:              7.0.1
+ * Version:              7.0.2
  * Text Domain:          simple-sales-tax
  * Domain Path:          /languages/
  *
