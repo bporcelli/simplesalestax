a49e7eb9b4ccc8bf16c8da31d0dca2fa686b2f02
Brett Porcelli <brettporcelli@gmail.com>
2023-04-12 11:01:33 -0400

    chore: bump tested WC version to 7.6

--- simple-sales-tax.php
+++ simple-sales-tax.php
@@ -13,7 +13,7 @@
  * Requires at least:    4.5.0
  * Tested up to:         6.1.0
  * WC requires at least: 3.0.0
- * WC tested up to:      7.4.0
+ * WC tested up to:      7.6.0
  *
  * @category             Plugin
  * @copyright            Copyright © 2023 The Federal Tax Authority, LLC
