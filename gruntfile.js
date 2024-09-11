85520e7cb6b0efa753fac9cdd7d82e03cdf1102f
Brett Porcelli <brettporcelli@gmail.com>
2024-09-11 12:29:15 -0400

    build: fix pot file generation

--- gruntfile.js
+++ gruntfile.js
@@ -4,25 +4,6 @@ module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({
-        makepot: {
-            target: {
-                options: {
-                    mainFile: 'simple-sales-tax.php',  // Main project file.
-                    type: 'wp-plugin',               // Type of project (wp-plugin or wp-theme).
-                    exclude: [
-                        'node_modules/.*',
-                        'includes/vendor/.*',
-                        'build/.*',
-                        'docker/.*',
-                    ],
-                    potHeaders: {
-                        'poedit': true,
-                        'report-msgid-bugs-to': 'https://github.com/bporcelli/simplesalestax/issues',
-                        'language-team': 'Brett Porcelli <bporcelli@taxcloud.com>'
-                    }
-                }
-            }
-        },
         uglify: {
             target: {
                 files: [{
@@ -49,12 +30,10 @@ module.exports = function (grunt) {
         },
     });
 
-    grunt.loadNpmTasks('grunt-wp-i18n');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     grunt.registerTask('build', ['uglify', 'cssmin']);
-    grunt.registerTask('i18n', ['makepot']);
     grunt.registerTask('default', ['build']);
 
 };
